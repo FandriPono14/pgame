102861e865c279c7a6ab2cafd4acb374212cfd47
FandriPono14 <56922611+FandriPono14@users.noreply.github.com>
2019-10-23 20:35:03 +0700

    code tugas 2

---
+++ pgame-16060/code_tugas2.py
@@ -0,0 +1,8 @@
+# Ini Tugas ke2. Praktek Git untuk MK Pemrograman game
+
+print ("Hello, My Name Is Fandri Pono") # Halo, Nama Saya Fandri Pono
+
+print ("I am Proud to Study to UMMU") # Saya Bangga Belajar di UMMU
+
+print ("Halo Mahasiswa Informatika.....") #baris ini tidak dieksikusi
+
