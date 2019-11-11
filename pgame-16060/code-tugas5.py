24c385da15d4deffb2eec55a518ebbc534d9dcca
FandriPono14 <56922611+FandriPono14@users.noreply.github.com>
2019-11-11 08:52:39 -0800

    Create code-tugas5.py

---
+++ pgame-16060/code-tugas5.py
@@ -0,0 +1,20 @@
+Latihan ke 8 
+#Boolean
+myvar=True          # Tipe data boolean
+print(myvar)        # Cetak Data
+True
+
+#Complex
+myvar =2+4j
+print(myvar)
+(2+4j)
+
+#Long
+myvar=888888888
+print(myvar)
+888888888
+
+#None
+myvar =None
+print(myvar)
+None
