fd97198cd281b34172ee57358ffb344d696a6a09
FandriPono14 <56922611+FandriPono14@users.noreply.github.com>
2019-12-06 05:36:22 -0800

    MODUL_PGAME_PRAKTIKUM_7

--- pgame-16060/code_tugas71.py
+++ pgame-16060/code_tugas71.py
@@ -1,9 +1,12 @@
+# Pemrograman Game Praktikum 7
+# latihan code 7.1 : PyGame
+
 import pygame, sys
 from pygame.locals import *
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((800, 600))
+DISPLAYSURF = pygame.display.set_mode((400, 300))
 
 pygame.display.set_caption('Hello World!')
 
